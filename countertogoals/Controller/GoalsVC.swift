07e87e8ef822e393160660f50992e2e0d7d0e001
ervinpepic <ervin.hack@gmail.com>
2018-01-13 16:17:53 +0100

    Designed user interface and added actions

---
+++ countertogoals/Controller/GoalsVC.swift
@@ -0,0 +1,38 @@
+//
+//  GoalsVC
+//  countertogoals
+//
+//  Created by Ervin on 13/01/2018.
+//  Copyright © 2018 Ervin Pepic. All rights reserved.
+//
+
+import UIKit
+
+class GoalsVC: UIViewController {
+    
+    
+    //Outlets
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    //Constants
+    
+    
+    //variables
+    
+    
+    //functions
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+    }
+
+    
+    //Actions
+    @IBAction func AddGoalButton(_ sender: Any) {
+        print("button Was Pressed")
+    }
+    
+
+}
+
